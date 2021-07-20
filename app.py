d403b122b19111398684a6064e2922ac71c671b8
Oliseh genesis <mwijusyaoliseh1@gmail.com>
2021-07-20 16:25:54 +0300

    finished login and sign up api

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def checkuser():
     body = request.get_json()
     email = body.get('email')
     password = body.get('password')
-    if collection.count_documents({"email": email , "password" : password}) > 0:
+    if collection.count_documents({"email": email , "password" : password}) == 1:
         return jsonify(
             success= 1 ,
             data = {'email': email,
@@ -30,13 +30,28 @@ def checkuser():
             error_message = "invalid creditials")
 
 @app.route('/signup', methods=['POST'])
-def insertUser():
-    user = request.get_json()
-    x = collection.insert_one(user)
-    return jsonify(
-        success = 0,
-        data = {'id' : x.inserted_id}
-    )
+def Createuser():
+    data = request.get_json()
+    email=data.get('email')
+    password = data.get('password')
+    phone= data.get('phone')
+    district = data.get('district')
+    user ={
+        "email" : email,
+        "password": password,
+        "phone": phone,
+        "district": district
+    }
+    if collection.count_documents({"email": email}) < 1:
+        collection.insert_one(user)
+        return jsonify(
+        success = 1,
+        message = "account successfully created"
+        )
+    else:
+        return jsonify(sucess = 0,
+        message = "email already in use"
+        )
 
     
 if __name__ == '__main__':
